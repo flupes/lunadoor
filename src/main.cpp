a030ba1cbdd1243e9cec2f3cb01d69a95ec93cb1
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-11-29 20:22:46 -0800

    copied example to check sensor

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,10 @@ void setup() {
   }
   Serial.println("LIS3DH found!");
 
-  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+  lis.setRange(LIS3DH_RANGE_4_G);
 
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());
+  Serial.print("Range = ");
+  Serial.print(2 << lis.getRange());
   Serial.println("G");
 
 
@@ -44,6 +45,21 @@ void setup() {
   }
   Serial.println();
 
+  Serial.print("Data rate set to: ");
+  switch (lis.getDataRate()) {
+    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
+    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
+    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
+    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
+    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
+    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
+
+    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
+  }
+
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println();
     Serial.println("WiFi Connected :-)");
@@ -55,4 +71,12 @@ void setup() {
 
 }
 
-void loop() {}
+void loop() {
+  lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+  Serial.print("X:  "); Serial.print(lis.x);
+  Serial.print("  \tY:  "); Serial.print(lis.y);
+  Serial.print("  \tZ:  "); Serial.println(lis.z);
+
+  delay(200);
+}
